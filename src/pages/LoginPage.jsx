0ee87a69dc66c447afb936f708198be2a06c2f7e
Arrianne <arrianneoshea@gmail.com>
2026-02-26 11:13:36 +1000

    adding the React routing dependencies so we can navigate through our app, removing boilerplate and adding basic info about our app

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,10 @@
 function LoginPage() {
-  return <h1>This is the login page.</h1>;
+  return (
+    <h1>
+      This is the login page but it's acting as the homepage until we actually
+      create one.
+    </h1>
+  );
 }
 
 export default LoginPage;
