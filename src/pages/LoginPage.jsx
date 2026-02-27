3d3c17504bb5ba5adac846e21d92d5d7c7f790aa
Arrianne <arrianneoshea@gmail.com>
2026-02-27 13:29:04 +1000

    adding the form to the login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
+import LoginForm from "../components/LoginForm";
+
 function LoginPage() {
   return (
-    <h1>
-      This is the login page but it's acting as the homepage until we actually
-      create one.
-    </h1>
+    <>
+      <h1>
+        This is the login page but it's acting as the homepage until we actually
+        create one.
+      </h1>
+      <LoginForm />
+    </>
   );
 }
 
